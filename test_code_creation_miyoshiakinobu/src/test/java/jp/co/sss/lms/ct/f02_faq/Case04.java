b69ec5126d5f9358b4c7d19faf86475b1fecaf49
edu <edu@WIN-P4HS74PTE6L.lan>
2026-03-23 11:02:41 +0900

    rename:フォルダ名　修正

---
+++ test_code_creation_miyoshiakinobu/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -0,0 +1,80 @@
+package jp.co.sss.lms.ct.f02_faq;
+
+import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+
+/**
+ * 結合テスト よくある質問機能
+ * ケース04
+ * @author holy
+ */
+@TestMethodOrder(OrderAnnotation.class)
+@DisplayName("ケース04 よくある質問画面への遷移")
+public class Case04 {
+
+	/** 前処理 */
+	@BeforeAll
+	static void before() {
+		createDriver();
+	}
+
+	/** 後処理 */
+	@AfterAll
+	static void after() {
+		closeDriver();
+	}
+
+	@Test
+	@Order(1)
+	@DisplayName("テスト01 トップページURLでアクセス")
+	void test01() {
+		//トップページにアクセス
+		goTo("http://localhost:8080/lms");
+		//タイトル一致確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
+	}
+
+	@Test
+	@Order(2)
+	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
+	void test02() {
+		//初回ログイン済の受講生ユーザでログイン
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA02");
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA02");
+		webDriver.findElement(By.className("btn")).click();
+
+		//遷移後、コース詳細画面のタイトル一致確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
+	}
+
+	@Test
+	@Order(3)
+	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
+	void test03() {
+		// TODO ここに追加
+	}
+
+	@Test
+	@Order(4)
+	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
+	void test04() {
+		// TODO ここに追加
+	}
+
+}
