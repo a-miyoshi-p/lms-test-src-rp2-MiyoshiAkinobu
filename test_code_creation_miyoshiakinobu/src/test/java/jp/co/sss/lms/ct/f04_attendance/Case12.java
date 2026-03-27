ccd5b08bce5963b1e806a0556b276aa876aec134
edu <edu@WIN-P4HS74PTE6L.lan>
2026-03-27 16:35:10 +0900

    rp:case12 作業報告
    テスト仕様書作成
    Case12.java Test1~4まで編集中
    Test3に関して修正中　過去日未入力アラートの処理関連
    エビデンス Case12_test01,02,04のみ

--- test_code_creation_miyoshiakinobu/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case12.java
+++ test_code_creation_miyoshiakinobu/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case12.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.netty.handler.timeout.TimeoutException;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,28 +43,71 @@ public class Case12 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページにアクセス
+		goTo("http://localhost:8080/lms");
+		//タイトル一致確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//初回ログイン済の受講生ユーザでログイン
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA02");
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA02");
+		webDriver.findElement(By.className("btn")).click();
+		//遷移後、コース詳細画面のタイトル一致確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//上部メニュー「勤怠」リンククリック
+		webDriver.findElement(By.linkText("勤怠")).click();
+		//過去日未入力状態の場合、ダイアログ表示されるので分岐処理
+		//try~catch,明示待機処理
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(2));
+		try {
+			//アラート待機　表示あり
+			wait.until(ExpectedConditions.alertIsPresent());
+			//ダイアログ表示されOKを押下
+			webDriver.switchTo().alert().accept();
+		} catch (TimeoutException e) {
+			//タイムアウトの場合継続
+		}
+
+		//遷移先タイトル一致確認
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「勤怠情報を直接編集する」リンクから勤怠情報直接変更画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		//「勤怠情報を直接編集する」リンクをクリック
+		webDriver.findElement(By.linkText("勤怠情報を直接編集する")).click();
+
+		//遷移先確認（Test03と同タイトル）
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+		//定時ボタンの存在で確認
+		assertTrue(webDriver.findElement(By.xpath("//button[text()='定時']")).isDisplayed());
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
